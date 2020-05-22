2400180900e5575891c23ce75709584f02356990
raoshi <2190796788@qq.com>
2020-05-22 13:31:54 +0800

    finished

--- linear.py
+++ linear.py
@@ -1,4 +1,36 @@
 import numpy as np
+import time
+
+class Vector():
+    def __init__(self, array):
+        self.array = array
+        self.row = len(array)
+        self.column = -1
+
+    def __str__(self):
+        c = "["
+        for i in range(self.row):
+            if i == self.row - 1:
+                c += " " + str(self.array[i])
+            elif i == 0:
+                c += str(self.array[i])
+            else:
+                c += " " + str(self.array[i])
+        c += "]"
+        return c
+
+    def innerProd(self, other):
+        c = 0
+        for i in range(0, self.row):
+            c += self.array[i] * other.array[i]
+        return c
+
+    def outerProd(self, other):
+        c = Matrix([[0 for j in range(self.row)] for i in range(self.row)])
+        for i in range(0, self.row):
+            for j in range(0, self.row):
+                c.array[i][j] = self.array[i] * other.array[j]
+        return c
 
 
 class Matrix():
@@ -6,7 +38,6 @@ class Matrix():
         self.array = array
         self.row = len(array)
         self.column = len(array[0])
-
 
     def __str__(self):
 
@@ -26,6 +57,8 @@ class Matrix():
                 extraspace = maxlen[j] - len(str(self.array[i][j]))
                 if j == self.column - 1:
                     a += " " * extraspace + str(self.array[i][j])
+                elif j == 0:
+                    a += " " + " " * extraspace + str(self.array[i][j]) + "  "
                 else:
                     a += " " * extraspace + str(self.array[i][j]) + "  "
             if i == self.row - 1:
@@ -33,7 +66,7 @@ class Matrix():
             else:
                 a += ']' + "\n"
         a += ']'
-        return a + "\n"
+        return a
 
     def __add__(self, other):
         c = Matrix([[0 for j in range(self.column)] for i in range(self.row)])
@@ -42,31 +75,79 @@ class Matrix():
                 c.array[i][j] = self.array[i][j] + other.array[i][j]
         return c
 
-    def __sub__ (self, other):
+    def __sub__(self, other):
         c = Matrix([[0 for j in range(self.column)] for i in range(self.row)])
         for i in range(0, self.row):
             for j in range(0, self.column):
                 c.array[i][j] = self.array[i][j] - other.array[i][j]
         return c
 
-    def __mul__ (self, other):
-        c = Matrix([[0 for j in range(other.column)] for i in range(self.row)])
-        for i in range(0, c.row):
-            for j in range(0, c.column):
-                c.array[i][j] = self.__mulEle(other, i, j)
-        return c
+    def __mul__(self, other):
+        if other.column == -1:
+            c = Vector([1 for i in range(self.row)])
+            for i in range(0, self.row):
+                c.array[i] = self.__mulVec(other, i)
+            return c
+        else:
+            c = Matrix([[0 for j in range(other.column)]
+                        for i in range(self.row)])
+            for i in range(0, c.row):
+                for j in range(0, c.column):
+                    c.array[i][j] = self.__mulEle(other, i, j)
+            return c
 
-    def __mulEle (self, other, i, j):
+    def __mulEle(self, other, i, j):
         ele = 0
         for k in range(0, self.column):
-                ele += self.array[i][k] * other.array[k][j]
+            ele += self.array[i][k] * other.array[k][j]
         return ele
 
+    def __mulVec(self, other, i):
+        ele = 0
+        for k in range(0, self.column):
+            ele += self.array[i][k] * other.array[k]
+        return ele
 
-a = Matrix([[1,2], [5, 0]])
-b = Matrix([[3, 4], [0, 2]])
+    def eleWise(self, other):
+        c = Matrix([[0 for j in range(self.column)] for i in range(self.row)])
+        for i in range(0, self.row):
+            for j in range(0, self.column):
+                c.array[i][j] = self.array[i][j] * other.array[i][j]
+        return c
 
 
-print(a + b)
-print(a - b)
-print(a * b)
+
+x = time.time()
+a = Matrix([[1, 2, 5], [5, 0, 2], [1, 3, 4]])
+b = Matrix([[3, 4, 1], [0, 2, 1], [2, 3, 7]])
+c = Vector([2, 3, 1])
+d = Vector([2, 4, 2])
+
+print("Mine:\n")
+print(a + b, "\n")
+print(a - b, "\n")
+print(a * b, "\n")
+print(a * c, "\n")
+print(Matrix.eleWise(a, b), "\n")
+print(Vector.innerProd(c, d), "\n")
+print(Vector.outerProd(c, d), "\n")
+y = time.time()
+print("-----" * 20)
+
+z = time.time()
+print("Numpy:\n")
+a = np.array([[1, 2, 5], [5, 0, 2], [1, 3, 4]])
+b = np.array([[3, 4, 1], [0, 2, 1], [2, 3, 7]])
+c = np.array([2, 3, 1])
+d = np.array([2, 4, 2])
+
+print(a + b, "\n")
+print(a - b, "\n")
+print(np.matmul(a, b), "\n")
+print(np.matmul(a, c), "\n")
+print(a * b, "\n")
+print(np.inner(c, d), "\n")
+print(np.outer(c, d), "\n")
+
+w = time.time()
+print(y - x,'s', w - z, 's')
